4419d1bdc25bea2e462fa0a97682041076b4fe5f
Thiago de Oliveira Alves <thiago.oalves246@gmail.com>
2023-07-09 17:23:26 -0300

    Desafios 3 e 5 resolvidos

--- PwcCodeChallenge/Program.cs
+++ PwcCodeChallenge/Program.cs
@@ -6,7 +6,12 @@ namespace PwcCodeChallenge {
             string phrase = "Hello, World! OpenAI is amazing.";
             //string phrase = Console.ReadLine();
             Console.WriteLine(PhraseInverter(phrase));
+            phrase = "Hello, World!";
             Console.WriteLine(RemoveDuplicatedChars(phrase));
+            phrase = "babad";
+            Console.WriteLine(PalindromeSubStr(phrase));
+            phrase = "racecar";
+            Console.WriteLine(IsPalindrome(phrase));
         }
 
         public static string PhraseInverter(string phrase) {
@@ -31,6 +36,52 @@ namespace PwcCodeChallenge {
                 phrase = phrase.Replace(phrase[i], aux1);
             }
             return result;
+        }
+
+        public static string PalindromeSubStr(string word) {
+            word = word.Trim();
+            string subStr;
+            string palindromeSubStr = "";
+            string aux1;
+            bool test1 = false; //used to test if a substring is palindrome
+            bool test2 = false; //true if a polindrome substring exist. Used to define the return
+
+            for (int i = 0; i < word.Length; i++) {
+                subStr = word[i].ToString().ToUpper();
+                aux1 = word[i].ToString();
+
+                for (int j = i+1; j < word.Length; j++) {
+                    subStr += word[j].ToString().ToUpper();
+                    aux1 += word[j].ToString();
+
+                    test1 = IsPalindrome(subStr);
+
+                    if (test1 == true && aux1.Length > palindromeSubStr.Length) {
+                        palindromeSubStr = aux1;
+                        test2 = true;
+                    }
+                }
+            }
+            if (test2 == true) {
+                return palindromeSubStr;
+            }
+            else {
+                return "A palavra não é palíndroma";
+            }
+        }
+
+        public static bool IsPalindrome (string word) {
+            bool test1 = false;
+            for (int i = 0; i < word.Length/2; i++) {
+                if (word[i] == word[word.Length - i - 1]) {
+                    test1 = true;
+                }
+                else {
+                    test1 = false;
+                    break;
+                }
+            }
+            return test1;
         }
     }
 }
